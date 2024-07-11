23b3470994e2b566aec7316e72397aaf2e0c9999
Trương Huỳnh Anh Dũng <truonghuynhanhdung@gmail.com>
2024-07-11 14:02:05 +0700

    TEM-24

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -30,7 +30,8 @@ class RouteServiceProvider extends ServiceProvider
             Route::middleware('api')
                 ->prefix('api')
                 ->group(base_path('routes/api.php'))
-                ->group(base_path('Middleware/AlwaysReturnJson.php'));
+                //->group(base_path('Middleware/AlwaysReturnJson.php'))
+                ;
 
             Route::middleware('web')
                 ->group(base_path('routes/web.php'));
